e65dac015896a804c98126e0e4fd2461fc0f52b9
Andre Oliveira <andreoliveira.adv@hotmail.com>
2023-01-14 17:48:10 -0500

    Validation in new user creation fields

--- helpdesk/src/main/java/com/arco/helpdek/resources/exceptions/ResourceExceptionHandler.java
+++ helpdesk/src/main/java/com/arco/helpdek/resources/exceptions/ResourceExceptionHandler.java
@@ -6,6 +6,8 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -25,5 +27,15 @@ public class ResourceExceptionHandler {
 	public ResponseEntity<StandardError> dataIntegrityViolationException(DataIntegrityViolationException ex, HttpServletRequest request) {
 		StandardError error = new StandardError(LocalDateTime.now(), HttpStatus.BAD_REQUEST.value(), "Violation", ex.getMessage(), request.getRequestURI());
 		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+	}
+	
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<StandardError> methodArgumentNotValidException(MethodArgumentNotValidException ex, HttpServletRequest request) {
+		ValidationError errors = new ValidationError(LocalDateTime.now(), HttpStatus.BAD_REQUEST.value(), "Validation error", "Field validation error", request.getRequestURI());
+		
+		for(FieldError fe : ex.getBindingResult().getFieldErrors()) {
+			errors.addErrors(fe.getField(), fe.getDefaultMessage());
+		}
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors);
 	}
 }

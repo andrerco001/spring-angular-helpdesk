15ce078c804e624111daf36a7e3654312a8c6e50
Andre Oliveira <andreoliveira.adv@hotmail.com>
2023-01-15 12:13:16 -0500

    create for Chamado

--- helpdesk/src/main/java/com/arco/helpdek/resources/ChamadoResource.java
+++ helpdesk/src/main/java/com/arco/helpdek/resources/ChamadoResource.java
@@ -1,14 +1,21 @@
 package com.arco.helpdek.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
+
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.arco.helpdek.domain.Chamado;
 import com.arco.helpdek.domain.dtos.ChamadoDTO;
@@ -32,5 +39,12 @@ public class ChamadoResource {
 		List<Chamado> chamados = chamadoService.fidAll();
 		List<ChamadoDTO> chamadosDTO = chamados.stream().map(c -> new ChamadoDTO(c)).collect(Collectors.toList());
 		return ResponseEntity.ok().body(chamadosDTO);
+	}
+	
+	@PostMapping
+	public ResponseEntity<ChamadoDTO> create(@Valid @RequestBody ChamadoDTO objDTO) {
+		Chamado obj = chamadoService.create(objDTO);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 }
--- helpdesk/src/main/java/com/arco/helpdek/services/ChamadoService.java
+++ helpdesk/src/main/java/com/arco/helpdek/services/ChamadoService.java
@@ -3,10 +3,17 @@ package com.arco.helpdek.services;
 import java.util.List;
 import java.util.Optional;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.arco.helpdek.domain.Chamado;
+import com.arco.helpdek.domain.Cliente;
+import com.arco.helpdek.domain.Tecnico;
+import com.arco.helpdek.domain.dtos.ChamadoDTO;
+import com.arco.helpdek.domain.enums.Prioridade;
+import com.arco.helpdek.domain.enums.Status;
 import com.arco.helpdek.repositories.ChamadoRepository;
 import com.arco.helpdek.services.exceptions.ObjectNotFoundException;
 
@@ -15,6 +22,10 @@ public class ChamadoService {
 
 	@Autowired
 	private ChamadoRepository chamadoRepository;
+	@Autowired
+	private TecnicoService tecnicoService;
+	@Autowired
+	private ClienteService clienteService;
 
 	public Chamado findById(Integer id) {
 		Optional<Chamado> obj = chamadoRepository.findById(id);
@@ -23,5 +34,27 @@ public class ChamadoService {
 
 	public List<Chamado> fidAll() {
 		return chamadoRepository.findAll();
+	}
+
+	public Chamado create(@Valid ChamadoDTO objDTO) {
+		return chamadoRepository.save(newChamado(objDTO));
+	}
+
+	private Chamado newChamado(ChamadoDTO obj) {
+		Tecnico tecnico = tecnicoService.findById(obj.getTecnico());
+		Cliente cliente = clienteService.findById(obj.getCliente());
+
+		Chamado chamado = new Chamado();
+		if (obj.getId() != null) {
+			chamado.setId(obj.getId());
+		}
+
+		chamado.setTecnico(tecnico);
+		chamado.setCliente(cliente);
+		chamado.setPrioridade(Prioridade.toEnum(obj.getPrioridade()));
+		chamado.setStatus(Status.toEnum(obj.getStatus()));
+		chamado.setTitulo(obj.getTitulo());
+		chamado.setObservacoes(obj.getObservacoes());
+		return chamado;
 	}
 }
